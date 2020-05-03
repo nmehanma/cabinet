77ab6901429e7396af5711d90d9eedddfd6dde9e
nmehanma <nmehanma@gmail.com>
2020-05-03 05:23:16 -0400

    doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,36 @@
+class DocsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new 
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_doc
+    
+  end
+
+  def doc_params
+
+  end
+end
