7c1cbc495767d39f64349c256fd0ed1cbb539884
nmehanma <nmehanma@gmail.com>
2020-05-05 02:35:58 -0400

    styling index with other updates

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,25 +2,27 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
-    @docs = Doc.where(user_id: current_user)
+    @docs = Doc.all.order("created_at DESC")
   end
 
   def show
- 
-    
   end
 
-  def new 
+  def new
     @doc = current_user.docs.build
   end
 
   def create
     @doc = current_user.docs.build(doc_params)
+
     if @doc.save
       redirect_to @doc
     else
       render 'new'
     end
+  end
+
+  def edit
   end
 
   def update
@@ -33,17 +35,15 @@ class DocsController < ApplicationController
 
   def destroy
     @doc.destroy
-    redirect_to docs_path 
-
+    redirect_to docs_path
   end
 
   private
+    def find_doc
+      @doc = Doc.find(params[:id])
+    end
 
-  def find_doc
-    @doc = Doc.find(params[:id])    
-  end
-
-  def doc_params
-    params.require(:doc).permit(:title, :content)
-  end
+    def doc_params
+      params.require(:doc).permit(:title, :content)
+    end
 end
